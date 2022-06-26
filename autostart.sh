933684f207cb0023c5094a9cda79ffe37a16c6c1
Sawyer Zheng <kmxsz@qq.com>
2022-06-27 00:04:46 +0800

    Git Init, a runnable QTile config

---
+++ autostart.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+picom --config  $HOME/.config/picom/picom.conf&
+sh -c "(sleep 10 && exec /opt/nutstore/bin/nutstore-pydaemon.py)"&
+"/home/sawyer/programs/nextcloud" --background&
+ibus-daemon -d&
